07d1ff71b9126210afcd32e2628d264751aed02c
lukkjkin <luckaslucas21@gmail.com>
2025-01-16 11:23:00 -0300

    primeiro commit

---
+++ sites/menu.js
@@ -0,0 +1,59 @@
+function MenuDrop_va(){
+    var arrow_right_va = document.getElementById("arrow_right_va")
+    var arrow_drop_va = document.getElementById("arrow_drop_va")
+    var ul_va = document.getElementById("ul_va")
+
+    if (arrow_right_va.style.display == "block"){
+        arrow_right_va.style.display = "none"
+        arrow_drop_va.style.display = "block"
+        ul_va.style.display = "block"
+    }else{
+        arrow_right_va.style.display = "block"
+        arrow_drop_va.style.display = "none"
+        ul_va.style.display = "none"
+    }
+}
+function MenuDrop_td(){
+    var arrow_right_td = document.getElementById("arrow_right_td")
+    var arrow_drop_td = document.getElementById("arrow_drop_td")
+    var ul_td = document.getElementById("ul_td")
+    if(arrow_right_td.style.display == 'block'){
+        arrow_right_td.style.display = "none"
+        arrow_drop_td.style.display = "block"
+        ul_td.style.display = "block"
+    }else{
+        arrow_right_td.style.display = "block"
+        arrow_drop_td.style.display = "none"
+        ul_td.style.display = "none"
+    }
+}
+function MenuDrop_co(){
+    var arrow_right_co = document.getElementById("arrow_right_co")
+    var arrow_drop_co = document.getElementById("arrow_drop_co")
+    var ul_co = document.getElementById("ul_co")
+
+    if (arrow_right_co.style.display == "block"){
+        arrow_right_co.style.display = "none"
+        arrow_drop_co.style.display = "block"
+        ul_co.style.display = "block"
+    }else{
+        arrow_right_co.style.display = "block"
+        arrow_drop_co.style.display = "none"
+        ul_co.style.display = "none"
+    }
+}
+function MenuDrop_re(){
+    var arrow_right_re = document.getElementById("arrow_right_re")
+    var arrow_drop_re = document.getElementById("arrow_drop_re")
+    var ul_re = document.getElementById("ul_re")
+
+    if (arrow_right_re.style.display == "block"){
+        arrow_right_re.style.display = "none"
+        arrow_drop_re.style.display = "block"
+        ul_re.style.display = "block"
+    }else{
+        arrow_right_re.style.display = "block"
+        arrow_drop_re.style.display = "none"
+        ul_re.style.display = "none"
+    }
+}
